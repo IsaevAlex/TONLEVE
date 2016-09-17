cdc9fedd60a1b9ed636c43262ed359caa45b5df3
Cronos <lexsandr46@gmail.com>
2016-09-17 21:01:15 +0300

    Add products, services , chat, favorites

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,7 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
+  config.assets.compile = true
   config.assets.raise_runtime_errors = true
     config.action_mailer.default :charset => "utf-8"
     config.action_mailer.perform_deliveries = true
